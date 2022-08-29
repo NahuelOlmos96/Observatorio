102402006ebbe3eb9f41203499dddc9ea51a07d0
Nahuel Olmos <nahuelolmos22@gmail.com>
2022-08-29 15:21:38 -0300

    primer commit con el navegador creado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import bootstrap from "bootstrap";
+import "./App.css";
+import {
+  BrowserRouter,
+  Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import { Home } from "./componentes/Home";
+import { Quines } from "./componentes/Quines";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <BrowserRouter>
+        
+          <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                Navbar
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link to="/">Home</Link>
+                  <Link to="/quines">quines somos</Link>
+                </div>
+              </div>
+            </div>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/quines" element={<Quines/>}></Route>
+          </Routes>
+        
+      </BrowserRouter>
     </div>
   );
 }
