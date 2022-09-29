753db33991aedebcfb47051853d4d495b1a434d9
Nahuel Olmos <nahuelolmos22@gmail.com>
2022-09-29 09:40:15 -0300

    "enlzes en programas"

--- src/componentes/Programas.jsx
+++ src/componentes/Programas.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
+import Flechasesion from "./Flechasesion";
+import datosProgramas from "../datos/programas.js";
+
 
 const Programas = () => {
-    return (
-        <div>
-            <h1>Programas</h1>
+  return (
+    <div className="contenedor">
+      <div className="fondoNoticias">
+        <div className="presentacio">
+          <Flechasesion nombreSecion="Programas" />
         </div>
-    );
+      </div>
+      <div className="contenedorCarrucel">
+        <div
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div class="carousel-inner Carrucel">
+            <div class="carousel-item active">
+              <img src={"https://i.ibb.co/K2NRg0b/Gacetilla-OTT-Traccionar-page-0001.jpg"} class="imagenCarrucel" alt="..." />
+             
+            </div>
+
+            {datosProgramas.map((programas) => (
+              <div class="carousel-item">
+                <img src={programas.imagen} class="imagenCarrucel" alt="..." />
+                
+              </div>
+            ))}
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Programas;
---
+++ src/datos/programas.js
@@ -0,0 +1,16 @@
+
+
+const datosProgramas = [
+  {
+    id: 1,
+    imagen: "https://i.ibb.co/wcY8xPD/Gacetilla-Soluciona-II-page-0001-1.jpg"
+    
+  },{
+    id: 2,
+    imagen: "https://i.ibb.co/JrsJ78V/Gacetilla-OTT-Producir-con-Equidad-page-0001.jpg"
+    
+  }
+   
+];
+
+export default datosProgramas;
